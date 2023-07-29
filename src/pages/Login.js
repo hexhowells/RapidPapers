78c106c4618b8ab6c41ece76dc36d9948b5822ce
Morgan <58524460+hexhowells@users.noreply.github.com>
2023-07-29 20:27:30 +0100

    Change Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,68 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 import axios from 'axios';
 
-function LoginScreen() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        try {
-            const response = await axios.post('/token', {
-                username: username,
-                password: password,
+class LoginScreen extends React.Component {
+    handleButtonClick = (provider) => {
+        axios.get(`/authorise/${provider}`)
+            .then(response => {
+                // handle successful response
+                // redirect user to new URL
+                window.location.href = response.data.redirect_url;
+            })
+            .catch(error => {
+                // handle error
+                console.log(error);
             });
-
-            if (response.status === 200) {
-                // Handle successful login here.
-                console.log('Login successful');
-            } else {
-                // Handle error here.
-                console.log('Login failed');
-            }
-        } catch (error) {
-            console.error(error);
-        }
     };
 
-    return (
-        <div className="container pt-4">
-            <div className="row justify-content-left">
-                <div className="col-12 col-md-6">
-                    <form onSubmit={handleSubmit}>
-                        <div className="form-group mb-4">
-                            <label htmlFor="username">Username</label>
-                            <input 
-                                type="text"
-                                className="form-control"
-                                id="username"
-                                placeholder="Username"
-                                value={username}
-                                onChange={e => setUsername(e.target.value)}
-                                required
-                            />
-                        </div>
-
-                        <div className="form-group mb-4">
-                            <label htmlFor="password">Password</label>
-                            <input 
-                                type="password"
-                                className="form-control"
-                                id="password"
-                                placeholder="Password"
-                                value={password}
-                                onChange={e => setPassword(e.target.value)}
-                                required
-                            />
-                        </div>
-
-                        <button type="submit" className="btn btn-primary">Log In</button>
-                    </form>
-                </div>
+    render() {
+        return (
+            <div className="container">
+                <button className="btn btn-primary m-2" onClick={() => this.handleButtonClick('google')}>
+                    Sign in with Google
+                </button>
+                <button className="btn btn-dark m-2" onClick={() => this.handleButtonClick('github')}>
+                    Sign in with GitHub
+                </button>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default LoginScreen;
