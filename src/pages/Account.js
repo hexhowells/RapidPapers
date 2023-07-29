ae839ae23c0eff523b8ff6e582a227001a8cd225
Morgan <58524460+hexhowells@users.noreply.github.com>
2023-07-29 20:29:46 +0100

    Display basic user information

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,11 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 const Account = () => {
-	return (
-		<>
-		<div className="container">
-			<h1>Account Page</h1>
-		</div>
-		</>
-		);
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        axios.get('/profile')
+        .then(response => {
+            setUser(response.data);
+            setLoading(false);
+        })
+        .catch(err => {
+            setError(err.toString());
+            setLoading(false);
+        });
+    }, []);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>{error}</p>;
+
+    return (
+        <div className="container">
+        	<h1 className="pb-3">Account Info</h1>
+            {user && (
+                <div>
+                    <p>Username: {user.username}</p>
+                    <p>Email Address: {user.email}</p>
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default Account;
