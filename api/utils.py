bb4b23afafadd9472a5449b2ffef78939e7be893
Morgan <58524460+hexhowells@users.noreply.github.com>
2023-08-12 23:19:02 +0100

    return extra information to user from backend

--- api/utils.py
+++ api/utils.py
@@ -1,11 +1,16 @@
 def paper_to_dict(paper):
-  return {
-      'id': paper[0],
-      'title': paper[1],
-      'abstract': paper[2],
-      'date': paper[3].strftime("%a, %d %b %Y"),
-      'categories': paper[4],
-      'authors': paper[5],
-      'arxiv_id': paper[6],
-      'upvotes': paper[7]
-      }
+	obj = {
+		'id': paper[0],
+		'title': paper[1],
+		'abstract': paper[2],
+		'date': paper[3].strftime("%a, %d %b %Y"),
+		'categories': paper[4],
+		'authors': paper[5],
+		'arxiv_id': paper[6],
+		'upvotes': paper[7]
+	}
+	if len(paper) == 10:
+		obj['upvote_status'] = paper[8]
+		obj['library_status'] = paper[9]
+
+	return obj
