7ea1450f2e1a0bb5c9b72d8ab643b57f15254181
Morgan <58524460+hexhowells@users.noreply.github.com>
2023-09-13 23:36:02 +0100

    Update about

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,12 +6,8 @@ const About = () => {
 			<h2 className="pt-3">About RapidPapers</h2>
 			<br></br>
 			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget pharetra elit,
-				 ac porttitor nulla. Nunc sollicitudin ut nisi quis congue. Etiam laoreet tortor 
-				 at nunc scelerisque pharetra. Quisque faucibus lectus in nibh tempus accumsan. 
-				 Curabitur euismod sapien at interdum convallis. Vestibulum hendrerit vel est eu 
-				 vulputate. Morbi vel massa pharetra eros gravida vestibulum id quis mi. Phasellus 
-				 et mi interdum, finibus mauris sed, consequat diam.
+				RapidPapers is a website that aims to make the process of finding and organising machine learning
+				research papers fast and simple.
 			</p>
 		</div>
 		</>
