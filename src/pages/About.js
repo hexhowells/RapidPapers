7c1d43aa1a55aa11021855a795e925d6506ac60e
Morgan <58524460+hexhowells@users.noreply.github.com>
2023-09-14 23:02:13 +0100

    Update about

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,6 +9,23 @@ const About = () => {
 				RapidPapers is a website that aims to make the process of finding and organising machine learning
 				research papers fast and simple.
 			</p>
+			<p>
+				Currently only supports arxiv papers, arxiv is pinged for new papers every hour however results 
+				are typically returned as batches once a day.
+			</p>
+			<p>
+				Currently indexed arxiv categories:
+				<ul>
+					<li>cs.AI</li>
+ 					<li>cs.CL</li>
+ 					<li>cs.CV</li>
+ 					<li>cs.LG</li>
+ 					<li>cs.MA</li>
+ 					<li>cs.NE</li>
+ 					<li>cs.RO</li>
+ 					<li>stat.ML</li>
+ 				</ul>
+			</p>
 		</div>
 		</>
 		);
