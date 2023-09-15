40c8d7254efdfddbcc342757b25f00fdcf30896d
Morgan <58524460+hexhowells@users.noreply.github.com>
2023-09-15 01:05:32 +0100

    Hide sort dropdown on main results page

--- src/components/HeaderResults.js
+++ src/components/HeaderResults.js
@@ -1,21 +1,23 @@
 import './HeaderResults.css';
 
-const HeaderResults = ({setSortType, setPageNum}) => {
+const HeaderResults = ({setSortType, setPageNum, showDropdown=true}) => {
 	return (
 		<>
 			<div className="container d-flex flex-wrap justify-content-center py-3">
 		        <h4 className="my-1 col-12 col-lg-auto mb-lg-0 me-lg-auto">Results</h4>
 		        
-	          	<div className="dropdown px-3">
-				    <select className="form-select" onChange={(event) => {
-				    		setSortType(event.target.value);
-				    		setPageNum(1);
-				    	}}>
-				    	<option value="relevant">Most relevant</option>
-				    	<option value="desc">Descending</option>
-			            <option value="asc">Ascending</option>
-			        </select>
-				</div>
+		        {showDropdown && (
+		          	<div className="dropdown px-3">
+					    <select className="form-select" onChange={(event) => {
+					    		setSortType(event.target.value);
+					    		setPageNum(1);
+					    	}}>
+					    	<option value="relevant">Most relevant</option>
+					    	<option value="desc">Descending</option>
+				            <option value="asc">Ascending</option>
+				        </select>
+					</div>
+				)}
 		    </div>
 		</>
 	);
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -12,13 +12,18 @@ const Main = () => {
   const [sortType, setSortType] = useState('relevant')
   const [pageNum, setPageNum] = useState(1);
   const [numPages, setNumPages] = useState(100);
+  const [showDropdown, setShowDropdown] = useState(false);
 
   useEffect(() => {
     var api_request;
-    if (searchQuery)
+    if (searchQuery) {
       api_request = `/api/v1/search?query=${searchQuery}&sort=${sortType}&page=${pageNum}`;
-    else
+      setShowDropdown(true);
+    }
+    else {
       api_request = `/api/v1/search?page=${pageNum}`;
+      setShowDropdown(false);
+    }
 
     fetch(api_request)
       .then((res) => res.json())
@@ -37,7 +42,7 @@ const Main = () => {
       <div className="container">
         <SearchBar setSearchQuery={setSearchQuery} setPageNum={setPageNum}></SearchBar>
 
-        <HeaderResults setSortType={setSortType} setPageNum={setPageNum}></HeaderResults>
+        <HeaderResults setSortType={setSortType} setPageNum={setPageNum} showDropdown={showDropdown}></HeaderResults>
         <ListResults results={results}></ListResults>
         <Pagination pageNum={pageNum} setPageNum={setPageNum} numPages={numPages}></Pagination>
       </div>
