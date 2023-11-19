aca9701e9c7e405806c58c698b215e3e5d05fecc
hexhowells <morganhowells314@gmail.com>
2023-11-19 23:07:20 +0000

    fix dropdown menu alignment on mobile

--- client/src/components/HeaderResults.js
+++ client/src/components/HeaderResults.js
@@ -3,11 +3,11 @@ import './HeaderResults.css';
 const HeaderResults = ({setSortType, setPageNum, showDropdown=true, headerTitle="Results"}) => {
 	return (
 		<>
-			<div className="container d-flex flex-wrap justify-content-center py-3">
-		        <h4 className="my-1 col-12 col-lg-auto mb-lg-0 me-lg-auto">{headerTitle}</h4>
+			<div className="container d-flex flex-wrap flex-row justify-content-between py-3">
+		        <h4 className="my-1 me-lg-auto">{headerTitle}</h4>
 		        
 		        {showDropdown && (
-		          	<div className="dropdown px-3">
+		          	<div className="dropdown">
 					    <select className="form-select" onChange={(event) => {
 					    		setSortType(event.target.value);
 					    		setPageNum(1);
@@ -24,3 +24,4 @@ const HeaderResults = ({setSortType, setPageNum, showDropdown=true, headerTitle=
 }
 
 export default HeaderResults;
+
