a78d83a87a35003f0a9b190ec7cf26693c16e113
hexhowells <morganhowells314@gmail.com>
2023-11-22 22:15:54 +0000

    update about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,34 +1,34 @@
+import React from 'react';
+import './About.css'; // Assuming you have a CSS file for styling
 
 const About = () => {
-	return (
-		<>
-		<div className="container">
-			<h2 className="pt-3">About RapidPapers</h2>
-			<br></br>
-			<p>
-				RapidPapers is a website that aims to make the process of finding and organising machine learning
-				research papers fast and simple.
-			</p>
-			<p>
-				Currently only supports arxiv papers, arxiv is pinged for new papers every hour however results 
-				are typically returned as batches once a day.
-			</p>
-			<p>
-				Currently indexed arxiv categories:
-				<ul>
-					<li>cs.AI</li>
- 					<li>cs.CL</li>
- 					<li>cs.CV</li>
- 					<li>cs.LG</li>
- 					<li>cs.MA</li>
- 					<li>cs.NE</li>
- 					<li>cs.RO</li>
- 					<li>stat.ML</li>
- 				</ul>
-			</p>
-		</div>
-		</>
-		);
+    return (
+        <div className="about-container">
+            <h2 className="about-header">Welcome to RapidPapers</h2>
+            <p className="about-intro">
+                RapidPapers is a website that aims to make the process of finding and organising machine learning
+                research papers fast and simple.
+            </p>
+            <div className="about-content">
+                <p>
+                    Currently supports arxiv papers, new papers are updated once daily.
+                </p>
+                <p>
+                    <strong>Organized Categories:</strong>
+                    <ul className="about-list">
+                        <li>cs.AI (Artificial Intelligence)</li>
+                        <li>cs.CL (Computation and Language)</li>
+                        <li>cs.CV (Computer Vision and Pattern Recognition)</li>
+                        <li>cs.LG (Machine Learning)</li>
+                        <li>cs.MA (Multiagent Systems)</li>
+                        <li>cs.NE (Neural and Evolutionary Computing)</li>
+                        <li>cs.RO (Robotics)</li>
+                        <li>stat.ML (Machine Learning Statistics)</li>
+                    </ul>
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default About;
