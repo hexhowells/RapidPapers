02fc7bc028260e405be079af3d1abb20b0386491
hexhowells <morganhowells314@gmail.com>
2023-12-09 18:51:35 +0000

    update styling and layout of account page

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -54,37 +54,46 @@ const Account = () => {
 	if (error) return <p>Error: {error}</p>;
 
 	return (
-			<div className="account-container">
-			{user && (
-					<div>
+		<div className="account-container">
+		{user && (
+				<div>
 					<div className="account-details">
-					<h2 className="account-heading">{user.username}</h2>
-					<p>Email Address: {user.email}</p>
-					<button className="btn btn-primary mt-4" onClick={handleLogout}>Logout</button>
+						<h3 className="account-heading">Account Details</h3>
+						<p>Username: {user.username}</p>
+						<p>Email Address: {user.email}</p>
+						<button className="btn btn-primary mt-4" onClick={handleLogout}>Logout</button>
+						
+						<h3 className="pt-5 pb-2">Change Password</h3>
+						<form onSubmit={handleChangePassword}>
+						<div className="form-group">
+							<label htmlFor="oldPassword">Old Password</label>
+							<input type="password" className="form-control" id="oldPassword" value={oldPassword} onChange={
+								(e) => setOldPassword(e.target.value)
+							} />
+						</div>
+						<div className="form-group">
+							<label htmlFor="newPassword">New Password</label>
+							<input type="password" className="form-control" id="newPassword" value={newPassword} onChange={
+								(e) => setNewPassword(e.target.value)
+							} />
+						</div>
+						<div className="form-group">
+							<label htmlFor="confirmNewPassword">Confirm New Password</label>
+							<input type="password" className="form-control" id="confirmNewPassword" value={newPassword} onChange={
+								(e) => setNewPassword(e.target.value)
+							} />
+						</div>
+						<button type="submit" className="btn btn-secondary mt-2">Change Password</button>
+						</form>
+					
+						<h3 className="mt-5 mb-3">Delete Account</h3>
+						<p>Warning! Once your account is deleted it will not be recovered!</p>
+						<button className="btn btn-danger mt-2" onClick={handleDeleteAccount}>Delete Account</button>
 					</div>
-
-					<div className="change-password">
-					<h5 className="pb-2">Change Password</h5>
-					<form onSubmit={handleChangePassword}>
-					<div className="form-group">
-					<label htmlFor="oldPassword">Old Password</label>
-					<input type="password" className="form-control" id="oldPassword" value={oldPassword} onChange={(e) => setOldPassword(e.target.value)} />
-					</div>
-					<div className="form-group">
-					<label htmlFor="newPassword">New Password</label>
-					<input type="password" className="form-control" id="newPassword" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} />
-					</div>
-					<button type="submit" className="btn btn-secondary mt-2">Change Password</button>
-					</form>
-					</div>
-
-					<div className="delete-account">
-						<button className="btn btn-danger mt-4" onClick={handleDeleteAccount}>Delete Account</button>
-					</div>
-					</div>
-					)}
-	</div>
-		);
+				</div>
+				)}
+		</div>
+	);
 };
 
 export default Account;
