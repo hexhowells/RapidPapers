c4d55b4baa5a910d12d5b8bf932828d8e4c6779c
hexhowells <morganhowells314@gmail.com>
2024-02-11 18:19:31 +0000

    show error message on failed login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './Login.css';
 
 class LoginScreen extends React.Component {
     state = {
         email: '',
-        password: ''
+        password: '',
+		loginError: false
     };
 
     handleButtonClick = (provider) => {
@@ -42,8 +43,12 @@ class LoginScreen extends React.Component {
 		// Send the login request to the server
 		fetch('/api/v1/login', requestOptions)
 			.then(response => {
-				console.log(response)
-				window.location.href = response.url;
+				if (response.status === 200) {
+					window.location.href = response.url;
+				} else {
+					this.setState(prevState => ({...prevState, loginError: true}));
+					console.log(this.state);
+				}
 			})
 			.catch(error => {
 				console.error('Login error:', error);
@@ -51,6 +56,7 @@ class LoginScreen extends React.Component {
 	};
 
     render() {
+		const { usr, pwd, loginError } = this.state;
         return (
             <div className="container d-flex justify-content-center align-items-center" style={{ 'margin-top': 'auto' }}>
                 <div className="card p-4" style={{ width: '400px' }}>
@@ -83,6 +89,11 @@ class LoginScreen extends React.Component {
                             </div>
                             <button type="submit" className="btn btn-primary w-100">Login</button>
                         </form>
+
+						{loginError &&
+							<p className="pt-2 text-danger">Username or password is incorrect</p>
+						}
+
                         <p className="mt-3 text-center">
                             New user? <a href="/signup">Create an account</a>
                         </p>
