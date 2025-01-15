e876b134b6ac58342ae3e3dd57ebb1467b321e62
hexhowells <morganhowells314@gmail.com>
2025-01-15 21:38:13 +0000

    remove default action for page buttons

--- client/src/components/Pagination.js
+++ client/src/components/Pagination.js
@@ -5,24 +5,27 @@ function Pagination({ pageNum, setPageNum, numPages }) {
     const lastPage = numPages;
     const startPage = Math.max(firstPage, pageNum - 2);
     const endPage = Math.min(lastPage, pageNum + 2);
-	
-	// Update page on change
-    const handlePageChange = (page) => {
+
+    // Update page on change
+    const handlePageChange = (page, event) => {
+        event.preventDefault(); // Prevent the default link behavior
         setPageNum(page);
-        window.scrollTo({top:0, behavior:'instant'});
+        window.scrollTo({ top: 0, behavior: 'instant' });
     };
 
-	// Goto next page
-    const handleNext = () => {
-        if (pageNum < lastPage) handlePageChange(pageNum + 1);
+    // Goto next page
+    const handleNext = (event) => {
+        event.preventDefault(); // Prevent default link behavior
+        if (pageNum < lastPage) handlePageChange(pageNum + 1, event);
     };
 
-	// Goto previous page
-    const handlePrev = () => {
-        if (pageNum > firstPage) handlePageChange(pageNum - 1);
+    // Goto previous page
+    const handlePrev = (event) => {
+        event.preventDefault(); // Prevent default link behavior
+        if (pageNum > firstPage) handlePageChange(pageNum - 1, event);
     };
 
-	// Get number of pages to display on the bottom of the screen
+    // Get number of pages to display on the bottom of the screen
     let pages = [];
     for (let i = startPage; i <= endPage; i++) {
         pages.push(i);
@@ -36,7 +39,9 @@ function Pagination({ pageNum, setPageNum, numPages }) {
                 </li>
 
                 <li className={`page-item ${firstPage === pageNum ? 'active' : ''}`}>
-                    <a className="page-link" href="/#" onClick={() => handlePageChange(firstPage)}>{firstPage}</a>
+                    <a className="page-link" href="/#" onClick={(e) => handlePageChange(firstPage, e)}>
+                        {firstPage}
+                    </a>
                 </li>
 
                 {startPage > firstPage + 1 && <li className="page-item"><span className="page-link">...</span></li>}
@@ -44,7 +49,7 @@ function Pagination({ pageNum, setPageNum, numPages }) {
                 {pages.map(page =>
                     page !== firstPage && page !== lastPage && (
                         <li className={`page-item ${page === pageNum ? 'active' : ''}`} key={page}>
-                            <a className="page-link" href="/#" onClick={() => handlePageChange(page)}>
+                            <a className="page-link" href="/#" onClick={(e) => handlePageChange(page, e)}>
                                 {page}
                                 {page === pageNum && <span className="sr-only"></span>}
                             </a>
@@ -55,7 +60,9 @@ function Pagination({ pageNum, setPageNum, numPages }) {
                 {endPage < lastPage - 1 && <li className="page-item"><span className="page-link">...</span></li>}
 
                 <li className={`page-item ${lastPage === pageNum ? 'active' : ''}`}>
-                    <a className="page-link" href="/#" onClick={() => handlePageChange(lastPage)}>{lastPage}</a>
+                    <a className="page-link" href="/#" onClick={(e) => handlePageChange(lastPage, e)}>
+                        {lastPage}
+                    </a>
                 </li>
 
                 <li className={`page-item ${pageNum === lastPage ? 'disabled' : ''}`}>
